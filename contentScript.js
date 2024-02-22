dee14ea9663f51f026cbf36bc2e7b243937078f3
Aafia Sait <aafiasait@cs.uchicago.edu>
2024-02-21 20:47:04 -0600

    files

---
+++ contentScript.js
@@ -0,0 +1,8 @@
+function scanText() {
+  const text = document.body.innerText;
+  console.log('Scanned Text:', text);
+  // Add logic here to process the text as needed
+}
+
+// Call scanText when the script is injected
+scanText();
